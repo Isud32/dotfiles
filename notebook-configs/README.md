72985a9867aecbd383e6f1151b13dd1ed3f46e6e
Isud <tobias_corrales@hotmail.com>
2026-08-02 16:08:22 -0300

    laptop dotfiles and pacman pkgs list that i use

---
+++ notebook-configs/README.md
@@ -0,0 +1,8 @@
+what's different from the other files is that this configs has keybinds in dwm for brightness and opening battery-menu, i added a keybind to change floating windows in normal dwm is super + shif + r here is super + f.
+
+
+- added a battery-menu for rofi and also made the app launcher and powermenu styles bigger.
+- made dwm bar font bigger.
+- added 2 dwmblocks scripts for battery and brightness
+- added bars for brightness and volume with xor
+
---
+++ notebook-configs/dwmblocks/config.h
@@ -0,0 +1,22 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+{"", "clock-2", 60, 1},
+  {"",	"capslock",	1,	6},
+  {"",	"clock",	60,	1},
+  {"",	"volume",	0,	2},
+	{"",	"internet",	5,	4},
+  /*{"", "archlogo", 5, 5},*/
+  /*{"", "dwmlogo", 5, 5}*/
+  {"", "sb-battery", 1, 7},
+  {"", "sb-brightness", 1, 8},
+  {"", "logopersec", 5, 5}
+};
+
+//Sets delimiter between status commands. NULL character ('\0') means no delimiter.
+static char *delim = "｜ ";
+
+// Have dwmblocks automatically recompile and run when you edit this file in
+// vim with the following line in your vimrc/init.vim:
+
+// autocmd BufWritePost ~/.local/src/dwmblocks/config.h !cd ~/.local/src/dwmblocks/; sudo make install && { killall -q dwmblocks;setsid dwmblocks & }
